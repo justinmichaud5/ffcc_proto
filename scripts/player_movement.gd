14f79f270e0c29ad2d82483a7fd873c08254df13
Justin <justin-michaud@hotmail.com>
2024-05-11 15:58:24 -0400

    Amélioration movement et rotation joueur

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -3,7 +3,10 @@ extends CharacterBody3D
 @onready var visuals = $Visuals
 
 const SPEED = 10.0
+const ACCELERATION = 0.5
+const DECCELERATION = 2.0
 const JUMP_VELOCITY = 4.5
+const TURN_SPEED = 0.4
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -17,11 +20,13 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-		visuals.rotation.y = Vector2(-input_dir.x, input_dir.y).angle()
+		velocity.x = move_toward(velocity.x, direction.x * SPEED, ACCELERATION)
+		velocity.z = move_toward(velocity.z, direction.z * SPEED, ACCELERATION)
+
+		var angle = Vector2(-input_dir.x, input_dir.y).angle()
+		visuals.rotation.y = lerp_angle(visuals.rotation.y, angle , TURN_SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, DECCELERATION)
+		velocity.z = move_toward(velocity.z, 0, DECCELERATION)
 
 	move_and_slide()
