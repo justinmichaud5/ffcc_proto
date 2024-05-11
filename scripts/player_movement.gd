3b4d1c443d7466fe7206c6403b9bbe23446ad05e
Justin <justin-michaud@hotmail.com>
2024-05-10 22:13:47 -0400

    ajout rotation de camera

---
+++ scripts/player_movement.gd
@@ -0,0 +1,27 @@
+extends CharacterBody3D
+
+@onready var visuals = $Visuals
+
+const SPEED = 10.0
+const JUMP_VELOCITY = 4.5
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+		visuals.rotation.y = Vector2(-input_dir.x, input_dir.y).angle()
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
